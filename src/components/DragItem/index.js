ce6e5d491bb3b11037b8d5d18e7396bddb235401
hanguyen2911 <nguyenthithuha2911ntth@gmail.com>
2023-03-28 15:25:33 +0700

    feat: create react app

--- src/components/DragItem/index.js
+++ src/components/DragItem/index.js
@@ -1,10 +1,10 @@
 import classNames from "classnames/bind";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Draggable from "react-draggable";
 
 import AddItem from "../AddItem";
 import styles from "./DragItem.module.scss";
-// import { Draggable } from "drag-react";
+
 const cx = classNames.bind(styles);
 
 function DragItem() {
@@ -13,18 +13,12 @@ function DragItem() {
     <>
       <AddItem onAddItem={(item) => setItems([...items, item])} />
       <div className={cx("App")}>
-        <div className={cx("App")}>
-          <div className={cx("container")}>
-            {items.map((item, index) => (
-              <Draggable
-                bounds="parent"
-    
-                key={index}
-              >
-                <div className={cx("item-container")}>{item}</div>
-              </Draggable>
-            ))}
-          </div>
+        <div className={cx("container")}>
+          {items.map((item, index) => (
+            <Draggable bounds="parent" key={index}>
+              <div className={cx("item-container")}>{item}</div>
+            </Draggable>
+          ))}
         </div>
       </div>
     </>
